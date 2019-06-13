931d6d8954fd1853d0b31fe3f8b29044d43e10f7
Daniel Hamill <hamill.daniel@gmail.com>
2019-06-13 17:09:24 -0400

    inital commit

---
+++ rascontrol/output_variables.py
@@ -0,0 +1,26 @@
+'''
+HECRAS Controller output variables
+
+Daniel Hamill
+
+'''
+
+
+output_variables = {"WSEL" : [2,"Water surface elevation calculated from energy equation"],
+"MIN_CH_EL" : [5,"Minimum channel elevation"],
+"STA_WS_LFT" : [36,"Left Station where water intersects the ground"],
+"STA_WS_RGT" : [37,"Right Station where water intersects the ground"],
+"FROUDE_CHL" : [48,"Froude number for channel" ],
+"FROUDE_XS" : [49,"Froude number for entire XS"],
+"HDYR_RADIUS" : [208, "Hydraulic Radius for the cross section"],
+"MAX_CH_DPTH" : [4 , "Maximum main channel Depth"],
+"Q_TOTAL" : [9,"Total flow in cross section"],
+"T_WIDTH" : [62,"Top width of the wetted XS"],
+"RIGHT_STA" : [264,  "Right station of a XS"],
+"LEFT_STA" : [263,  "Left station of a XS"],
+"CH_STA_L" : [158,  "Left station of channel"],
+"CH_STA_R" : [159,  " Right station of channel"],
+"ICE_BTM_CHAN":[194,"The bottom elevation of ice in the main channel"],
+"CRIT_DEPTH":[58,"Critical depth, corresponds to the critical water surface"],
+"CRIT_WS":[56,"Critical water surface elevation"]
+}
